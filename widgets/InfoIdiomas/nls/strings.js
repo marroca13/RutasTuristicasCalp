ebd164bc1c6db91c2aad22067a6269e8183f62f8
marroca13 <mr13mar@gmail.com>
2019-04-07 19:46:51 +0200

    Copied files

---
+++ widgets/InfoIdiomas/nls/strings.js
@@ -0,0 +1,10 @@
+﻿define({
+    root:({
+      titulo: "¡Descubre los secretos de Calp!",
+      parrafo1: "Encuentra los rincones más emblematícos de la ciudad: sus calles, su historia, su hermosas vistas panorámicas, su naturaleza y mucho más. Para ampliar tu conocimiento accede al link que aparece más abajo",
+      parrafo2: "Calp es una invitación para tus sentidos. Despierta. Empiezas un nuevo día. Olvídate del tiempo. Estás sumergido en el Mediterráneo. El azul del agua. El sol. La arena en tus pies. Respira. Calp huele a sal. Viaja por nuestras calles. Son rutas de pescadores que cuentan nuestras historias. Encuentra nuestro espacio natural. Calp es un refugio para tu viaje.",
+      parrafo3: "El Peñón de Ifach custodia nuestras culturas. Descubre sus sendas. Encuentra sus tesoros. Se respira silencio. La tierra cuenta sus leyendas. Las voces perdidas del Peñón reclaman nuestra atención. Ya formas parte de nuestra historia. Haz de nuestro legado tu mejor viaje.",
+      parrafo4: "¡Pincha aquí para más información!"
+      }),
+
+});
